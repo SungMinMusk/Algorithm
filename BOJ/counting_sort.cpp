f28e1eb9275454269433811e7fb5ff810bd35312
Kim Seong Min <sm3487@naver.com>
2018-10-08 23:34:55 +0900

    Create counting_sort.cpp

---
+++ BOJ/counting_sort.cpp
@@ -0,0 +1,57 @@
+/*
+  문제 : https://www.acmicpc.net/problem/10989
+  풀이 : counting sort를 이용한다.
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<stack>
+#include<string>
+#include<set>
+#include<map>
+#include<queue>
+#include<cstdio>
+#include<math.h>
+#include<string.h>
+#include<float.h>
+#define SWS ios_base::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0)
+#define endl '\n'
+#define FOR(a,b,c) for(int a=b;a<=c;++a)
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef stack<int> sti;
+typedef queue<int> qi;
+typedef priority_queue<int> pqi;
+typedef stack<pair<int, int> > stii;
+typedef queue<pair<int, int> > qii;
+typedef priority_queue<pair<int, int> > pqii;
+typedef pair<int, int > pii;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<string> vs;
+typedef vector<pair<int, int > >vii;
+typedef set<int> seti;
+const int INF = 0x7fffffff;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int dz[2] = { -1, 1 };
+/* -----구현부----- */
+int arr[10001];
+int main()
+{
+	SWS;
+	int n;
+	cin >> n;
+	FOR(i, 1, n)
+	{
+		int a;
+		cin >> a;
+		arr[a]++;
+	}
+	FOR(i, 1, 10000)
+		FOR(j, 1, arr[i])
+			cout << i << endl;
+
+	return 0;
+}
