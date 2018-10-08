77b383e80ae381c870f6311aca32badd2ed9763d
Kim Seong Min <sm3487@naver.com>
2018-10-08 23:38:16 +0900

    Create easy.cpp

---
+++ BOJ/NO.9085/easy.cpp
@@ -0,0 +1,58 @@
+/*
+  문제 : https://www.acmicpc.net/problem/9085
+  풀이 : 입력받으면서 바로 더해준 결과를 출력한다.
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<stack>
+#include<string>
+#include<set>
+#include<map>
+#include<queue>
+#include<cstdio>
+#include<math.h>
+#include<string.h>
+#include<float.h>
+#define SWS ios_base::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0)
+#define endl '\n'
+#define FOR(a,b,c) for(int a=b;a<=c;++a)
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef stack<int> sti;
+typedef queue<int> qi;
+typedef priority_queue<int> pqi;
+typedef stack<pair<int, int> > stii;
+typedef queue<pair<int, int> > qii;
+typedef priority_queue<pair<int, int> > pqii;
+typedef pair<int, int > pii;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<string> vs;
+typedef vector<pair<int, int > >vii;
+typedef set<int> seti;
+const int INF = 0x7fffffff;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int dz[2] = { -1, 1 };
+/* -----구현부----- */
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int a, sum = 0;
+		cin >> a;
+		while (a--)
+		{
+			int tmp;
+			cin >> tmp;
+			sum +=tmp;
+		}
+		cout << sum << endl;
+	}
+
+	return 0;
+}
