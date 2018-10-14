0492d8a340ce897a22d5070f5db4dde1e86e4616
Kim Seong Min <sm3487@naver.com>
2018-10-15 02:07:05 +0900

    Update bruteforce.cpp

--- BOJ/NO.12886/bruteforce.cpp
+++ BOJ/NO.12886/bruteforce.cpp
@@ -5,33 +5,16 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-#include<stack>
 #include<string>
-#include<set>
 #include<map>
-#include<queue>
-#include<cstdio>
 #include<math.h>
 #include<string.h>
-#include<float.h>
 #define SWS ios_base::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0)
 #define endl '\n'
 #define FOR(a,b,c) for(int a=b;a<=c;++a)
 using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
-typedef stack<int> sti;
-typedef queue<int> qi;
-typedef priority_queue<int> pqi;
-typedef stack<pair<int, int> > stii;
-typedef queue<pair<int, int> > qii;
-typedef priority_queue<pair<int, int> > pqii;
-typedef pair<int, int > pii;
-typedef vector<int> vi;
-typedef vector<long long> vll;
-typedef vector<string> vs;
-typedef vector<pair<int, int > >vii;
-typedef set<int> seti;
 const int INF = 0x7fffffff;
 int dx[4] = { 1,-1,0,0 };
 int dy[4] = { 0,0,1,-1 };
