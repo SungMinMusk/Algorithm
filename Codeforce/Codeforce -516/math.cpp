30733861b0805aa7387dacc006a999cc785f68dd
Kim Seong Min <sm3487@naver.com>
2018-10-18 01:39:02 +0900

    Create math.cpp

---
+++ Codeforce/Codeforce -516/math.cpp
@@ -0,0 +1,50 @@
+/*
+  문제 : http://codeforces.com/contest/1064/problem/A
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<stack>
+#include<string>
+#include<set>
+#include<map>
+#include<queue>
+#include<cstdio>
+#include<math.h>
+#include<string.h>
+#include<float.h>
+#define SWS ios_base::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0)
+#define endl '\n'
+#define FOR(a,b,c) for(int a=b;a<=c;++a)
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef stack<int> sti;
+typedef queue<int> qi;
+typedef priority_queue<int> pqi;
+typedef stack<pair<int, int> > stii;
+typedef queue<pair<int, int> > qii;
+typedef priority_queue<pair<int, int> > pqii;
+typedef pair<int, int > pii;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<string> vs;
+typedef vector<pair<int, int > >vii;
+typedef set<int> seti;
+const int INF = 0x7fffffff;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int dz[2] = { -1, 1 };
+/* -----구현부----- */
+int main()
+{
+	SWS;
+	int arr[3];
+	cin >> arr[0] >> arr[1] >> arr[2];
+	sort(arr, arr + 3);
+	int sum = arr[0] + arr[1];
+	if (sum > arr[2]) cout << 0;
+	else cout << arr[2] - sum + 1;
+
+	return 0;
+}
