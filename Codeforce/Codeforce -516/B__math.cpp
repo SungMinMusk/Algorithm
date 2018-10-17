7244d8b404d5377730cbfafd23060d05db62fc05
Kim Seong Min <sm3487@naver.com>
2018-10-18 01:40:21 +0900

    Create B__math.cpp

---
+++ Codeforce/Codeforce -516/B__math.cpp
@@ -0,0 +1,55 @@
+/*
+  문제 : http://codeforces.com/contest/1064/problem/B
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<stack>
+#include<string>
+#include<set>
+#include<map>
+#include<queue>
+#include<cstdio>
+#include<math.h>
+#include<string.h>
+#include<float.h>
+#define SWS ios_base::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0)
+#define endl '\n'
+#define FOR(a,b,c) for(int a=b;a<=c;++a)
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef stack<int> sti;
+typedef queue<int> qi;
+typedef priority_queue<int> pqi;
+typedef stack<pair<int, int> > stii;
+typedef queue<pair<int, int> > qii;
+typedef priority_queue<pair<int, int> > pqii;
+typedef pair<int, int > pii;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<string> vs;
+typedef vector<pair<int, int > >vii;
+typedef set<int> seti;
+const int INF = 0x7fffffff;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int dz[2] = { -1, 1 };
+/* -----구현부----- */
+int main()
+{
+	SWS;
+	int n;
+	cin >> n;
+	while (n--)
+	{
+		ll a;
+		cin >> a;
+		int sum = 0;
+		FOR(i, 0, 31)
+			if (a & (1 << i))
+				sum++;
+		cout << (1L << sum) << endl;
+	}
+	return 0;
+}
