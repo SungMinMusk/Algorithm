37e3f08f11c932e39b220527fdfd8962e78f3557
Kim Seong Min <sm3487@naver.com>
2018-11-03 20:58:13 +0900

    Create simulation.cpp

---
+++ BOJ/NO.2460/simulation.cpp
@@ -0,0 +1,58 @@
+/*
+  문제 : https://www.acmicpc.net/problem/2460
+  풀이 : 승하차시마다 결과를 갱신해준다.
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<stack>
+#include<string>
+#include<set>
+#include<map>
+#include<queue>
+#include<cstdio>
+#include<math.h>
+#include<string.h>
+#include<float.h>
+#define SWS ios_base::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0)
+#define endl '\n'
+#define FOR(a,b,c) for(int a=b;a<=c;++a)
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int > pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<pair<int, int > >vii;
+typedef stack<int> sti;
+typedef stack<pair<int, int> > stii;
+typedef queue<int> qi;
+typedef queue<pair<int, int> > qii;
+typedef priority_queue<int> pqi;
+typedef priority_queue<pair<int, int> > pqii;
+typedef map<int, int> mapii;
+typedef map<string, int> mapsi;
+typedef set<int> seti;
+const int INF = 0x7fffffff;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int dz[2] = { -1, 1 };
+/* -----구현부----- */
+
+int main()
+{
+	SWS;
+	int res = -1;
+	int sum = 0;
+	FOR(i, 1, 10)
+	{
+		int a, b;
+		cin >> a >> b;
+		sum -= a;
+		res = max(res, sum);
+		sum += b;
+		res = max(res, sum);
+	}
+	cout << res;
+	return 0;
+}
