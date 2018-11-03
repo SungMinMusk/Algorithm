322d9587345203a48f75ecbe0f08875b986d2990
Kim Seong Min <sm3487@naver.com>
2018-11-03 21:03:08 +0900

    Create simulation.cpp

---
+++ BOJ/NO.2525/simulation.cpp
@@ -0,0 +1,58 @@
+/*
+  문제 : https://www.acmicpc.net/problem/2525
+  풀이 : 시간의 단위 변화를 잘 처리한다.
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<stack>
+#include<string>
+#include<set>
+#include<map>
+#include<queue>
+#include<cstdio>
+#include<math.h>
+#include<string.h>
+#include<float.h>
+#define SWS ios_base::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0)
+#define endl '\n'
+#define FOR(a,b,c) for(int a=b;a<=c;++a)
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int > pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<pair<int, int > >vii;
+typedef stack<int> sti;
+typedef stack<pair<int, int> > stii;
+typedef queue<int> qi;
+typedef queue<pair<int, int> > qii;
+typedef priority_queue<int> pqi;
+typedef priority_queue<pair<int, int> > pqii;
+typedef map<int, int> mapii;
+typedef map<string, int> mapsi;
+typedef set<int> seti;
+const int INF = 0x7fffffff;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int dz[2] = { -1, 1 };
+/* -----구현부----- */
+int main()
+{
+	SWS;
+	int t, m, a;
+	cin >> t >> m >> a;
+	FOR(i, 1, a)
+	{
+		m++;
+		if (m == 60)
+		{
+			m = 0;
+			t++;
+		}
+		if (t == 24) t = 0;
+	}
+	cout << t << " " << m;
+	return 0;
+}
