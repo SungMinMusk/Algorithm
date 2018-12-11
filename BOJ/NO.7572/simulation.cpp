57218b52ba27d4ef567fc6c2da2b1064da250bd4
Kim Seong Min <sm3487@naver.com>
2018-12-11 16:21:51 +0900

    Create simulation.cpp

---
+++ BOJ/NO.7572/simulation.cpp
@@ -0,0 +1,57 @@
+/*
+  문제 : https://www.acmicpc.net/problem/7572
+  풀이 : 1년이 갑자가 아님에 주의.
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<stack>
+#include<string>
+#include<set>
+#include<map>
+#include<queue>
+#include<cstdio>
+#include<math.h>
+#include<string.h>
+#include<float.h>
+#define SWS ios_base::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0)
+#define endl '\n'
+#define FOR(a,b,c) for(int a=b;a<=c;++a)
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef stack<int> sti;
+typedef queue<int> qi;
+typedef priority_queue<int> pqi;
+typedef stack<pair<int, int> > stii;
+typedef queue<pair<int, int> > qii;
+typedef priority_queue<pair<int, int> > pqii;
+typedef pair<int, int > pii;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<string> vs;
+typedef vector<pair<int, int > >vii;
+typedef set<int> seti;
+const int INF = 0x7fffffff;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int dz[2] = { -1, 1 };
+/* -----구현부----- */
+int a=7;
+char b='J';
+
+int main()
+{
+	SWS;
+	int n;
+	cin >> n;
+	FOR(i, 1, n-1)
+	{
+		a++;
+		b++;
+		if (a == 10) a = 0;
+		if (b == 'M') b = 'A';
+	}
+	cout << b << a;
+	return 0;
+}
